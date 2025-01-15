c5d8f94f7e8a81f43cf2d1b812b94e829bf63ba2
jfranco <jefersonfranco@pm.me>
2025-01-15 17:04:49 -0300

    fix(markdown): update footer.tsx

--- src/app/_components/footer.tsx
+++ src/app/_components/footer.tsx
@@ -7,7 +7,7 @@ export function Footer() {
       <Container>
         <div className="py-28 flex flex-col lg:flex-row items-center">
           <h3 className="text-4xl lg:text-[2.5rem] font-bold tracking-tighter leading-tight text-center lg:text-left mb-10 lg:mb-0 lg:pr-4 lg:w-1/2">
-            Statically Generated with Next.js, TailwindCSS, and TypeScript.
+            Statically Generated with Next.js, TailwindCSS, Markdown, and TypeScript.
           </h3>
           <div className="flex justify-center space-x-4 py-6">
             <a href="https://github.com/jeferson-franco">GitHub</a>
