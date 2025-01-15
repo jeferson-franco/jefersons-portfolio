9f7d56f698064d44cefdefd14f916f2b9a2a1f21
jfranco <jefersonfranco@pm.me>
2025-01-15 17:01:56 -0300

    wip(Update the Main Page): update page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,30 +1,14 @@
-import Container from "@/app/_components/container";
-import { HeroPost } from "@/app/_components/hero-post";
-import { Intro } from "@/app/_components/intro";
-import { MoreStories } from "@/app/_components/more-stories";
-import { getAllPosts } from "@/lib/api";
+import Navigation from "./_components/navigation";
+import Hero from "./_components/hero";
+import Projects from "./_components/projects";
 
-export default function Index() {
-  const allPosts = getAllPosts();
-
-  const heroPost = allPosts[0];
-
-  const morePosts = allPosts.slice(1);
-
+export default function Home() {
   return (
     <main>
-      <Container>
-        <Intro />
-        <HeroPost
-          title={heroPost.title}
-          coverImage={heroPost.coverImage}
-          date={heroPost.date}
-          author={heroPost.author}
-          slug={heroPost.slug}
-          excerpt={heroPost.excerpt}
-        />
-        {morePosts.length > 0 && <MoreStories posts={morePosts} />}
-      </Container>
+      <Navigation />
+      <Hero />
+      <Projects />
+      {/* Add other sections */}
     </main>
   );
 }
