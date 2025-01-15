35d173e5f07c84da449978b22686a87f9b6d9ab4
jfranco <jefersonfranco@pm.me>
2025-01-15 17:00:38 -0300

    wip(create a Hero component): feat: create hero.tsx

---
+++ src/app/_components/hero.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+export default function Hero() {
+  return (
+    <section>
+      <h1 className="text-6x1 font-bold">Jeferson Franco</h1>
+      <h2 className="text-2x1 mt-4">Software Engineer</h2>
+      <p className="mt-6 text-x1">Building amazing web experiences</p>
+    </section>
+  );
+}
